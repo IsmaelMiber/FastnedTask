7f8ec107baf09cf35d75986320e9d9056176bcb8
Ismael Miber <ismael.miber@elcoach.me>
2023-09-30 22:32:21 +0300

    [ADD] react-query pkg and it's eslint plugin.

--- app/FastnedTask/.eslintrc.js
+++ app/FastnedTask/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  plugins: ['@tanstack/query'],
+  extends: [
+    '@react-native',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
 };
