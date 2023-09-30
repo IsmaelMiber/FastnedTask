927fc180e63cff1d9e399c6ced9a395d1a3f72b1
Ismael Miber <ismael.miber@elcoach.me>
2023-09-30 22:29:24 +0300

    [ADD] Main navigation stack that includes main screens.

---
+++ app/FastnedTask/src/navigation/Main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Vehicles from '../screens/Vehicles/Vehicles';
+import VehicleDetails from '../screens/VehicleDetails/VehicleDetails';
+import Charging from '../screens/Charging/Charging';
+
+const Stack = createNativeStackNavigator();
+
+const screenOptions = {
+  headerShown: false,
+};
+
+export default function Main() {
+  return (
+    <Stack.Navigator initialRouteName="Vehicles" screenOptions={screenOptions}>
+      <Stack.Screen name="Vehicles" component={Vehicles} />
+      <Stack.Screen name="VehicleDetails" component={VehicleDetails} />
+      <Stack.Screen name="Charging" component={Charging} />
+    </Stack.Navigator>
+  );
+}
