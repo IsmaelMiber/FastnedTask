7886825f6f02d3f337756b047bf20ef444072a19
Ismael Miber <ismael.miber@elcoach.me>
2023-10-06 18:22:49 +0300

    [ADD] Charging screen and styles.

--- app/FastnedTask/src/screens/Charging/Charging.styles.ts
+++ app/FastnedTask/src/screens/Charging/Charging.styles.ts
@@ -1,5 +1,62 @@
 import {StyleSheet} from 'react-native';
+import FONTS from '../../constants/fonts';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+  },
+  chargingBarContainer: {
+    marginTop: 25,
+  },
+  barBody: {
+    height: 50,
+  },
+  barItem: {
+    height: 50,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+  },
+  shadow: {
+    alignSelf: 'center',
+    marginTop: 22,
+    height: 10,
+    width: '90%',
+    backgroundColor: 'rgba(256, 256, 256, 0.1)',
+    borderRadius: 10 / 2,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 5,
+
+    elevation: 12,
+  },
+  chargingDetailsContainer: {
+    marginTop: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  chargingPercentage: {
+    fontFamily: FONTS.SemiBold,
+    fontSize: 36,
+    color: 'rgb(146, 209, 72)',
+    textAlign: 'center',
+  },
+  label: {
+    fontFamily: FONTS.Medium,
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  value: {
+    fontFamily: FONTS.Regular,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
 
 export default styles;
--- app/FastnedTask/src/screens/Charging/Charging.tsx
+++ app/FastnedTask/src/screens/Charging/Charging.tsx
@@ -1,10 +1,72 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Screen from '../Screen/Screen';
+import styles from './Charging.styles';
+import LottieView from 'lottie-react-native';
+import VehicleOverview from '../Vehicles/components/VehicleOverview/VehicleOverview';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../types/Navigation';
+import useCharging from './hooks/useCharging';
 
-export default function Charging() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Charging'>;
+
+function Charging({route}: Props) {
+  const {params} = route;
+  const {vehicle} = params;
+
+  const {chargeSpeedInKw, progress, energyProgress, percentage, stopAnimation} =
+    useCharging(vehicle);
   return (
-    <View>
-      <Text>Charging</Text>
-    </View>
+    <Screen>
+      <View style={styles.container}>
+        <VehicleOverview asView vehicle={vehicle} />
+        <View style={styles.chargingBarContainer}>
+          {/* Bar Top */}
+          <LottieView
+            source={require('../../assets/lotties/barTop.json')}
+            style={styles.barItem}
+            progress={progress}
+            loop={false}
+          />
+          {/* Bar Body */}
+          <LottieView
+            source={require('../../assets/lotties/barBody.json')}
+            style={styles.barBody}
+            progress={progress}
+            loop={false}
+          />
+          <View style={styles.shadow} />
+          {/* Bar Thunder */}
+          <LottieView
+            source={require('../../assets/lotties/barThunder.json')}
+            autoPlay
+            loop={stopAnimation}
+            style={styles.barItem}
+          />
+          {/* Bar Shine */}
+          <LottieView
+            source={require('../../assets/lotties/barShine.json')}
+            autoPlay
+            loop={stopAnimation}
+            style={styles.barItem}
+            speed={percentage <= 80 ? 0.25 : 1}
+          />
+        </View>
+
+        <View style={styles.chargingDetailsContainer}>
+          <View>
+            <Text style={styles.label}>kWh</Text>
+            <Text style={styles.value}>{energyProgress}</Text>
+          </View>
+          <Text style={styles.chargingPercentage}>{percentage}%</Text>
+          <View>
+            <Text style={styles.label}>Speed</Text>
+            <Text style={styles.value}>{chargeSpeedInKw} kW</Text>
+          </View>
+        </View>
+      </View>
+    </Screen>
   );
 }
+
+export default Charging;
